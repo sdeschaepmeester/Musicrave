74dca69a030f98c16c0cd876c8033bcf5425a09e
Samantha D <deschaepmeester.samantha@gmail.com>
2022-09-14 19:02:39 +0200

    Add get album's tracks api call

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,17 +1,31 @@
 import '../App.css';
 import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function Tracks(props) {
-    // const getArtistTracks = async () => {
-    //     const requestURL = "https://api.spotify.com/v1/artists/"+props.artistId+"/albums"
-    //     const {data} = await axios.get(requestURL, {
-    //         headers: {
-    //             Authorization: `Bearer ${props.token}`
-    //         }
-    //     })
-    //     console.log(data)
-    // }
-    // getArtistTracks();
+
+    useEffect(() => {
+        getArtistTracks();
+    }, [])
+
+    const getArtistTracks = async () => {
+
+       console.log("faire la requete get artists albums")
+       // 6LArUyIoMiHmDvvOAsE2cd
+
+       /////////////////////////
+       ///////////////////////// Get an album track: for album with ID 2Y5WftWX9ZA7a01NYaBX4L
+       ///////////////////////// It works ! do not delete.
+       // get an album tracks : album id : 2Y5WftWX9ZA7a01NYaBX4L
+       
+    //    const {data} = await axios.get("https://api.spotify.com/v1/albums/2Y5WftWX9ZA7a01NYaBX4L/tracks", {
+    //     headers: {
+    //         Authorization: `Bearer ${props.token}`
+    //     }
+    // })
+    // console.log(data)
+
+    }
     return (
     <div className="Album">
         <h1 className="title-section">Explore the artist's albums !</h1>
